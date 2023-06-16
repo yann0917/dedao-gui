7886c9ad43a9562f2a13f242405b7523118302b6
Yabo <386139859@qq.com>
2023-06-16 08:52:11 +0800

    :art: update wails

--- frontend/wailsjs/go/backend/App.d.ts
+++ frontend/wailsjs/go/backend/App.d.ts
@@ -3,13 +3,13 @@
 import {services} from '../models';
 import {backend} from '../models';
 
-export function AlgoFilter(arg1:services.AlgoFilterParam):Promise<any>;
+export function AlgoFilter(arg1:services.AlgoFilterParam):Promise<services.AlgoFilterResp>;
 
-export function AlgoProduct(arg1:services.AlgoFilterParam):Promise<any>;
+export function AlgoProduct(arg1:services.AlgoFilterParam):Promise<services.AlgoProductResp>;
 
 export function ArticleDetail(arg1:number,arg2:string):Promise<string>;
 
-export function ArticleList(arg1:string,arg2:string,arg3:number,arg4:number):Promise<any>;
+export function ArticleList(arg1:string,arg2:string,arg3:number,arg4:number):Promise<services.ArticleList>;
 
 export function CheckLogin(arg1:string,arg2:string):Promise<backend.LoginResult>;
 
@@ -17,52 +17,52 @@ export function CourseCategory():Promise<Array<services.CourseCategory>>;
 
 export function CourseDownload(arg1:number,arg2:number,arg3:number,arg4:string):Promise<void>;
 
-export function CourseInfo(arg1:string):Promise<any>;
+export function CourseInfo(arg1:string):Promise<services.CourseInfo>;
 
-export function CourseList(arg1:string,arg2:string,arg3:number,arg4:number):Promise<any>;
+export function CourseList(arg1:string,arg2:string,arg3:number,arg4:number):Promise<services.CourseList>;
 
-export function EbookCommentList(arg1:string,arg2:number,arg3:number):Promise<any>;
+export function EbookCommentList(arg1:string,arg2:number,arg3:number):Promise<services.EbookCommentList>;
 
 export function EbookDownload(arg1:number,arg2:number,arg3:string):Promise<void>;
 
-export function EbookInfo(arg1:string):Promise<any>;
+export function EbookInfo(arg1:string):Promise<services.EbookDetail>;
 
-export function EbookShelfAdd(arg1:Array<string>):Promise<any>;
+export function EbookShelfAdd(arg1:Array<string>):Promise<services.EbookShelfAddResp>;
 
-export function EbookUserInfo():Promise<any>;
+export function EbookUserInfo():Promise<services.EbookVIPInfo>;
 
 export function GetHomeInitialState():Promise<services.HomeInitState>;
 
 export function GetQrcode():Promise<backend.QrCodeResp>;
 
-export function GetVolcPlayAuthToken(arg1:string,arg2:string):Promise<any>;
+export function GetVolcPlayAuthToken(arg1:string,arg2:string):Promise<services.MediaVolc>;
 
-export function GetVolcPlayInfo(arg1:string):Promise<any>;
+export function GetVolcPlayInfo(arg1:string):Promise<services.VodPlayInfoResp>;
 
 export function Logout():Promise<void>;
 
-export function NotesTimeline(arg1:string):Promise<any>;
+export function NotesTimeline(arg1:string):Promise<services.NotesTimeline>;
 
-export function OdobDownload(arg1:number,arg2:number,arg3:any):Promise<void>;
+export function OdobDownload(arg1:number,arg2:number,arg3:services.Course):Promise<void>;
 
-export function OdobUserInfo():Promise<any>;
+export function OdobUserInfo():Promise<services.OdobVip>;
 
 export function OpenDirectoryDialog(arg1:string):Promise<string>;
 
-export function SearchHot():Promise<any>;
+export function SearchHot():Promise<services.SearchTot>;
 
 export function SetDir(arg1:Array<string>):Promise<void>;
 
-export function SunflowerLabelContent(arg1:string,arg2:number,arg3:number,arg4:number):Promise<any>;
+export function SunflowerLabelContent(arg1:string,arg2:number,arg3:number,arg4:number):Promise<services.SunflowerContent>;
 
-export function SunflowerLabelList(arg1:number):Promise<any>;
+export function SunflowerLabelList(arg1:number):Promise<services.SunflowerLabelList>;
 
-export function SunflowerResourceList():Promise<any>;
+export function SunflowerResourceList():Promise<services.SunflowerResourceList>;
 
-export function TopicAll(arg1:number,arg2:number):Promise<any>;
+export function TopicAll(arg1:number,arg2:number):Promise<services.TopicAll>;
 
-export function TopicNoteDetail(arg1:string):Promise<any>;
+export function TopicNoteDetail(arg1:string):Promise<services.TopicDetail>;
 
-export function TopicNotesList(arg1:string,arg2:boolean,arg3:number,arg4:number):Promise<any>;
+export function TopicNotesList(arg1:string,arg2:boolean,arg3:number,arg4:number):Promise<services.NotesList>;
 
-export function UserInfo():Promise<any>;
+export function UserInfo():Promise<services.User>;
--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13
 	github.com/mitchellh/mapstructure v1.5.0
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
-	github.com/wailsapp/wails/v2 v2.4.1
+	github.com/wailsapp/wails/v2 v2.5.1
 	golang.org/x/sync v0.1.0
 )
 
