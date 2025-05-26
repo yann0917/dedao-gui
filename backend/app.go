f279cf805e0175c01b7f1e6345c71441a99edcbb
Yabo <386139859@qq.com>
2025-05-27 07:53:02 +0800

    :art:

--- backend/app.go
+++ backend/app.go
@@ -2,6 +2,12 @@ package backend
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/yann0917/dedao-gui/backend/utils"
 )
 
 // App struct
@@ -22,9 +28,32 @@ func (a *App) Startup(ctx context.Context) {
 
 func (a *App) Shutdown(ctx context.Context) {
 	// fmt.Println(a.Ctx)
+	setupCleanupOnExit()
 }
 
 func (a *App) DomReady(ctx context.Context) {
 	// fmt.Println(a.Ctx)
 	// fmt.Println("dom ready")
+}
+
+func setupCleanupOnExit() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-c
+		fmt.Println("正在关闭程序...")
+
+		// 获取 BadgerDB 实例并关闭
+		db, err := utils.GetBadgerDB(utils.GetDefaultBadgerDBPath())
+		if err == nil && db != nil {
+			if err := db.Close(); err != nil {
+				fmt.Printf("关闭数据库时出错: %v\n", err)
+			} else {
+				fmt.Println("数据库已安全关闭")
+			}
+		}
+
+		os.Exit(0)
+	}()
 }
