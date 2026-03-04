e5040f569c5360ec36462292559d0bcedc6b4a92
Yabo <386139859@qq.com>
2026-03-04 19:56:18 +0800

    :art:

--- backend/services/article.go
+++ backend/services/article.go
@@ -11,7 +11,7 @@ type ArticleDetail struct {
 
 // Content article Content
 type Content struct {
-	Aid      string      `json:"aid"`
+	Aid      interface{} `json:"aid"`
 	AliasID  string      `json:"aliasId"`
 	Contents interface{} `json:"contents"`
 	Desc     string      `json:"desc"`
